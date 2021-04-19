cc58e8dab8b95b4d27b6efb59daa9bd11113608f
Skyler Chiu <s35chiu@uwaterloo.ca>
2021-04-19 00:05:55 -0400

    Addition of flight animation and pipes

--- game.js
+++ game.js
@@ -2,10 +2,10 @@ const cvs = document.getElementById("bird");
 const ctx = cvs.getContext("2d");
 
 let frames = 0;
-
+const DEGREE = Math.PI/180;
 
 const sprite = new Image();
-sprite.src = "images/sprite.png"
+sprite.src = "images/sprite.png";
 
 const state = {
     current : 0,
@@ -55,10 +55,18 @@ const fg = {
     x:0,
     y:cvs.height - 112,
 
+    dx: 2,
+
     draw :function (){
         ctx.drawImage (sprite, this.sX, this.sY, this.w, this.h, this.x, this.y
             , this.w, this.h);
         ctx.drawImage (sprite, this.sX, this.sY, this.w, this.h, this.x+ this.w, this.y, this.w, this.h);
+    },
+
+    update : function(){
+        if (state.current == state.game){
+            this.x = (this.x- this.dx)%(this.w/2);
+        }
     }
    
 }
@@ -82,13 +90,17 @@ const bird = {
     gravity: 0.25,
     jump: 4.6,
     speed:0,
+    rotation: 0,
 
 
     draw : function (){
         let bird = this.animation[this.frame];
 
-        ctx.drawImage (sprite, bird.sX, bird.sY, this.w, this.h, this.x - this.w/2,  this.y - this.h/2
-            , this.w, this.h);
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.rotation);
+        ctx.drawImage (sprite, bird.sX, bird.sY, this.w, this.h, - this.w/2,  - this.h/2, this.w, this.h);
+        ctx.restore();
     },
 
     flap : function (){
@@ -105,6 +117,7 @@ const bird = {
 
         if (state.current == state.getReady){
             this.y = 150;
+            this.rotation = 0 * DEGREE;
         }
         else{
             this.speed += this.gravity;
@@ -116,9 +129,18 @@ const bird = {
                     state.current = state.over;
                 }
             }
+
+            if (this.speed >= this.jump){ //Means the bird is falling down
+                this.rotation = 90 * DEGREE;
+                this.frame = 1;
+            } else {
+                this.rotation = -25 * DEGREE;
+            }
         }
     }
 }
+
+
 
 const getReady = {
     sX:0,
@@ -152,11 +174,74 @@ const gameOver = {
     }
 }
 
+const pipes = {
+    position : [],
+    
+    top : {
+        sX : 553,
+        sY : 0
+    },
+    bottom:{
+        sX : 502,
+        sY : 0
+    },
+    
+    w : 53,
+    h : 400,
+    gap : 85,
+    maxYPos : -150,
+    dx : 2,
+
+   draw : function(){
+        for (let i = 0; i < this.position.length; i++){
+            let p = this.position[i];
+
+            let topYPos = p.y;
+            let bottomYPos = p.y + this.h + this.gap;
+            
+            //top pipe
+            ctx.drawImage (sprite, this.top.sX, this.top.sY, this.w, this.h, p.x, topYPos, this.w, this.h);
+            //bottom pipe
+            ctx.drawImage (sprite, this.bottom.sX, this.bottom.sY, this.w, this.h, p.x, bottomYPos, this.w, this.h);
+
+        }
+    },
+  
+ 
+    
+    update: function(){
+        if(state.current !== state.game) return;
+        
+        if(frames%100 == 0){
+            this.position.push({
+                x : cvs.width,
+                y : this.maxYPos * ( Math.random() + 1)
+            });
+        }
+        for(let i = 0; i < this.position.length; i++){
+            let p = this.position[i];
+
+            // COLLISION DETECTION
+            // TOP PIPE
+
+            
+            // MOVE THE PIPES TO THE LEFT
+            p.x -= this.dx;
+            
+            // if the pipes go beyond canvas, we delete them from the array
+
+        }
+    },
+
+    
+}
+
 function draw(){
     ctx.fillStyle = "#70c5ce";
     ctx.fillRect(0, 0, cvs.width, cvs.height);
 
     bg.draw();
+    pipes.draw();
     fg.draw();
     bird.draw();
     getReady.draw();
@@ -165,7 +250,9 @@ function draw(){
 }
 
 function update(){
-    bird.update()
+    bird.update();
+    fg.update();
+    pipes.update();
 }
 
 
